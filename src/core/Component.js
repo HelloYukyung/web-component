07490d58dc111d7edc1ca6cf80f568033e934c96
yukyung123 <rladbrud12@naver.com>
2023-02-21 22:32:03 +0900

    feat  :  컴포넌트 분할하기 - 기능추가

--- src/core/Component.js
+++ src/core/Component.js
@@ -24,4 +24,13 @@ export default class Component {
     this.state = { ...this.state, ...newState };
     this.render();
   }
+
+  addEvent(eventType, selector, callback) {
+    const children = [...this.$target.querySelectorAll(selector)];
+    this.$target.addEventListener(eventType, (event) => {
+      if (!event.target.closest(selector)) return false;
+      //closest 일치하는 요소를 찾을 때까지 자기 자신을 포함해 위쪽으로 문서 트리를 순회
+      callback(event);
+    });
+  }
 }
